58bd23aeb4b6bf39bd950bcf468b0c2f5c649e69
Anselm Kiefner <fuzzy@anselm.kiefner.de>
2019-07-21 15:49:03 +0200

    hedges should work with functions now

--- src/fuzzylogic/hedges.py
+++ src/fuzzylogic/hedges.py
@@ -2,34 +2,48 @@
 """
 Lingual hedges modify curves of membership values.
 
-These work with sets only. It's more trouble than it is worth making
-these work with pure functions, so meta-functionality was removed.
+These should work with Sets and functions.
 """
 
 from .classes import Set
 
 def very(g):
     """Sharpen memberships so that only the values close 1 stay at the top."""
-    def s_f(g):
+    if isinstance(g, Set):
+        def s_f(g):
+            def f(x):
+                return g(x) ** 2
+            return f
+        return Set(s_f(g.func))
+    else:
         def f(x):
             return g(x) ** 2
         return f
-    return Set(s_f(g.func))
 
 
 def plus(g):
     """Sharpen memberships like 'very' but not as strongly."""
-    def s_f(g):
+    if isinstance(g, Set):
+        def s_f(g):
+            def f(x):
+                return g(x) ** 1.25
+            return f
+        return Set(s_f(g.func))
+    else:
         def f(x):
             return g(x) ** 1.25
         return f
-    return Set(s_f(g.func))
     
 
 def minus(g):
     """Increase membership support so that more values hit the top."""
-    def s_f(g):
+    if isinstance(g, Set):
+        def s_f(g):
+            def f(x):
+                return g(x) ** 0.75
+            return f 
+        return Set(s_f(g.func))
+    else:
         def f(x):
             return g(x) ** 0.75
-        return f 
-    return Set(s_f(g.func))
+        return f
