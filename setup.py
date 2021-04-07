d0f0bb0313cca6e691c0fae91172a55c2ade373b
Anselm Kiefner <fuzzy@anselm.kiefner.de>
2021-04-07 10:21:02 +0200

    patch version push

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ meta={
     "description":"Fuzzy Logic for Python 3",
     "license":"MIT",
     "url":"https://github.com/amogorkon/fuzzylogic",
-    "version":"1.0.0",
+    "version":"1.0.1",
     "author":"Anselm Kiefner",
     "author_email":"fuzzylogic-pypi@anselm.kiefner.de",
-    "python_requires":">3.6",
+    "python_requires":">=3.8",
     "keywords":["fuzzy logic",],
     "classifiers":[
         "Development Status :: 4 - Beta",
